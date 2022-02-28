6235021ccd6705d341109437d8a5ee2141f72e78
Your Name <you@example.com>
2022-02-28 16:56:09 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-
+import data from './data'
+import Qestion from './Qestion';
 function App() {
+ const [questions ,setQuestions] = useState(data)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+        <div className="container">
+          <h3>question and answers about login</h3>
+          <section className="info">
+            {questions.map((question)=>{
+              return(
+                <Qestion key={question.id} {...question}/>
+              )
+            })}
+          </section>
+        </div>
+    </main>
   );
 }
 
